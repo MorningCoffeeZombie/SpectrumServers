6f096e7b7e9a26be900e5298f26b391c2a7b53f5
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2020-12-23 18:20:36 -0600

    Better eng translations

--- clanorg/lua/orgs/sh_orglang.lua
+++ clanorg/lua/orgs/sh_orglang.lua
@@ -1,12 +1,12 @@
 ORGS_Lang = {}
 
 ORGS_Lang.orgleft = " left the organization!"
-ORGS_Lang.orgleft1 = "You left the organization!"
+ORGS_Lang.orgleft1 = "You have left the organization!"
 
-ORGS_Lang.havenoorg = "You don't have an organization!"
+ORGS_Lang.havenoorg = "you don't have an organization!"
 
-ORGS_Lang.neworg = "Congratulations, your organization has been created.!"
-ORGS_Lang.neworg1 ="Enter! Orghelp to learn how to manage your new organization."
+ORGS_Lang.neworg = "Congratulations, your organization was created!"
+ORGS_Lang.neworg1 ="Enter !orghelp to learn about how to manage your new organization."
 
 ORGS_Lang.cantafford = "You can't afford it."
 
@@ -16,129 +16,130 @@ ORGS_Lang.alreadyin = "You are already a member of the organization."
 ORGS_Lang.alreadyin1 = "Leave the old organization to create a new one!"
 ORGS_Lang.alreadyin2 = "This player is already in your organization!"
 
-ORGS_Lang.sent = "the invitation is sent!"
-ORGS_Lang.cant = "You cannot invite yourself!"
-ORGS_Lang.leader = "You must be the creator of the organization!"
+ORGS_Lang.sent = "Invitation sent!"
+ORGS_Lang.cant = "You can't invite yourself!"
+ORGS_Lang.leader = "You must be the Creator of the organization!"
 
-ORGS_Lang.leaving = "* Leave the old organization *"
+ORGS_Lang.leaving = "*Leave the old organization*"
 
 ORGS_Lang.accept = "You have chosen to accept the invitation, welcome to your new organization!"
-ORGS_Lang.joined = "Joined the organization!"
+ORGS_Lang.joined = " joined the organization!"
 
-ORGS_Lang.notinvited = "Nobody invited you."
+ORGS_Lang.notinvited = "no one invited You."
 ORGS_Lang.deny = "You have chosen to cancel the invitation."
 
-ORGS_Lang.plnomoney = "You don't have enough money!"
-ORGS_Lang.orgnomoney = "There is no such amount in the organization's bank."
+ORGS_Lang.plnomoney = "you don't have enough money!"
+ORGS_Lang.orgnomoney = "there is no such amount In the organization's Bank."
 ORGS_Lang.validnum = "You must enter a valid value!"
-ORGS_Lang.nopermission = "Not enough rights for this"
 
-ORGS_Lang.motdchange = "MOTD Changed Successfully!"
+ORGS_Lang.nopermission = "Not enough permissions for this"
 
-ORGS_Lang.offine = "Member is offline"
-ORGS_Lang.memberkicked = "was kicked from the organization!"
-ORGS_Lang.youkicked = "You were kicked from the organization!"
-ORGS_Lang.cantkickowner = "You cannot kick the creator of the organization!"
+ORGS_Lang.motdchange = "MOTD you've Successfully changed!"
+
+ORGS_Lang.offine = "Member not online"
+ORGS_Lang.memberkicked = " was kicked out of the organization!"
+ORGS_Lang.youkicked = "You were kicked out of the organization!"
+ORGS_Lang.cantkickowner = "You can't kick the organization's Creator!"
 
 
-ORGS_Lang.cantrankowner = "You cannot assign a rank to the creator of an organization!"
+ORGS_Lang.cantrankowner = "You can't assign a title to the Creator of an organization!"
 
-ORGS_Lang.rankcreate = "Rank has been created!"
-ORGS_Lang.rankupdate = "Rank has been updated!"
-ORGS_Lang.rankdelete = "The title has been deleted!"
+ORGS_Lang.rankcreate = "Rank created!"
+ORGS_Lang.rankupdate = "Rank updated!"
+ORGS_Lang.rankdelete = "Rank deleted!"
 
-ORGS_Lang.rankowner = "Creator"
-ORGS_Lang.ranknewmember = "New Member"
+ORGS_Lang.rankowner = "Creator "
+ORGS_Lang.ranknewmember = "New member"
 
-ORGS_Lang.orghelp1 = ORGS_Config.addonName .. "Commands"
-ORGS_Lang.orghelp2 = "! Orgmenu - To open the organization menu"
-ORGS_Lang.orghelp3 = "! Orgc [Text] - To speak in the organization's chat"
+ORGS_Lang.orghelp1 = ORGS_Config.addonName .." Commands"
+ORGS_Lang.orghelp2 = "!orgmenu -  to open the organization's menu"
+ORGS_Lang.orghelp3 = "!orgc [Text] - to talk to the organization's chat"
 
 ORGS_Lang.dashboard = "Information"
-ORGS_Lang.members = "Members"
-ORGS_Lang.membername = "Member Name"
-ORGS_Lang.memberrank = "Member Rank"
-ORGS_Lang.isonline = "Currently online"
-ORGS_Lang.lastseen = "Last Visited"
+ORGS_Lang.members = "   Members"
+ORGS_Lang.membername = "member Name"
+ORGS_Lang.memberrank = "member Rank"
+ORGS_Lang.isonline = "Right now online"
+ORGS_Lang.lastseen = "Last visit"
 ORGS_Lang.yes = "Yes"
-ORGS_Lang.no = "No"
+ORGS_Lang.no = "Not"
 
-ORGS_Lang.updaterank = "Change Rank"
+ORGS_Lang.updaterank = "Change rank"
 
 ORGS_Lang.options = "Options"
-ORGS_Lang.optionholo = "Backlight"
-ORGS_Lang.optionchat = "Chat"
+ORGS_Lang.optionholo = "Highlight"
+ORGS_Lang.optionchat = "     Chat"
 
 ORGS_Lang.optionleave = "Leave"
 ORGS_Lang.warning = "Warning!"
-ORGS_Lang.leave = "Are you sure you want to leave" ..ORGS_Config.addonName .. "?"
+ORGS_Lang.leave = "Are you sure you want to leave "..ORGS_Config.addonName.."?"
 
-ORGS_Lang.bank = "Bank"
-ORGS_Lang.bankwelcome = "Welcome to the bank!"
-ORGS_Lang.bankbalance = "Balance:"
-ORGS_Lang.bankhowmuchdeposit = [[How much do you want to deposit?]]
-ORGS_Lang.bankhowmuchwithdraw = [[How much do you want to withdraw?]]
+ORGS_Lang.bank = "        Bank"
+ORGS_Lang.bankwelcome = "Welcome to the Bank!"
+ORGS_Lang.bankbalance = "Balance: "
+ORGS_Lang.bankhowmuchdeposit = [[how Much do you want to Deposit?]]
+ORGS_Lang.bankhowmuchwithdraw = [[how Much do you want to withdraw?]]
 ORGS_Lang.bankdeposit = "Deposit"
 ORGS_Lang.bankwithdraw = "Withdraw"
 ORGS_Lang.bankback = "Back"
 
-ORGS_Lang.manage = "Settings"
-ORGS_Lang.rankname = "Rank name .."
+ORGS_Lang.manage = "   Settings"
+ORGS_Lang.rankname = "Rank Name.."
 
-ORGS_Lang.flags = "Rank Flags - For example - a, b, c, d"
+ORGS_Lang.flags = "Rank Flags - For example - a,b,c,d"
 ORGS_Lang.permissions = "Permission"
 ORGS_Lang.flagsdeposit = "Deposit money"
-ORGS_Lang.flagswithdraw = "Withdraw Money"
-ORGS_Lang.flagsinvite = "Invite Players"
+ORGS_Lang.flagswithdraw = "Withdraw money"
+ORGS_Lang.flagsinvite = "Invite players"
 ORGS_Lang.flagsmotd = "Change MoTD"
-ORGS_Lang.flagskick = "Exclude Players"
-ORGS_Lang.flagsranks = "Change Rank"
-ORGS_Lang.flagsranks1 = "Manage Ranks"
+ORGS_Lang.flagskick = "Kick players"
+ORGS_Lang.flagsranks = "Change rank"
+ORGS_Lang.flagsranks1 = "Manage ranks"
 
 ORGS_Lang.createrank = "Create rank!"
 ORGS_Lang.notallowed = "This name is not allowed!"
-ORGS_Lang.createranks = "Create Rank"
-ORGS_Lang.selectrank = "Choose your rank!"
+ORGS_Lang.createranks = "Create rank"
+ORGS_Lang.selectrank = "Select rank!"
 ORGS_Lang.editrank = "Change rank!"
 ORGS_Lang.deleterank = "Delete rank!"
 
-ORGS_Lang.editranks = "Change Rank"
+ORGS_Lang.editranks = "Change the rank"
 
-ORGS_Lang.chooserank = "Choose a rank ..."
+ORGS_Lang.chooserank = "Select a rank..."
 
 ORGS_Lang.changemotd = "Change MoTD!"
 
-ORGS_Lang.limit = "You have exceeded the letter limit! (Limit 350)"
+ORGS_Lang.limit = "You have increased the limit of letters! (Limit 350)"
 
-ORGS_Lang.chooseplayer = "Select a player"
+ORGS_Lang.chooseplayer = "Select player"
 ORGS_Lang.needchooseplayer = "You must choose a player!"
 
-ORGS_Lang.inviteplayer = "Invite a player!"
+ORGS_Lang.inviteplayer = "Invite player!"
 ORGS_Lang.invite = "Invite"
 
 ORGS_Lang.invited = "You were invited to ".. ORGS_Config.addonName .."!"
 
 ORGS_Lang.acceptinvite = "Accept"
-ORGS_Lang.denyinvite = "Deny"
+ORGS_Lang.denyinvite = "Reject"
 
-ORGS_Lang.holoon = "Organization player lighting is on. (Only works if players are online)"
-ORGS_Lang.holooff = "Organization player lighting is off. (Only works if players are online)"
+ORGS_Lang.holoon = "Highlighting the players included. (Only works if players are online)"
+ORGS_Lang.holooff = "the organization's player Highlighting is disabled. (Only works if players are online)"
 
-ORGS_Lang.chaton = "Organization chat is enabled."
-ORGS_Lang.chatoff = "Organization chat is disabled."
+ORGS_Lang.chaton =  "The chat of the organization turn on."
+ORGS_Lang.chatoff =  "The organization's chat is turn off."
 
-ORGS_Lang.meetexplain = "Here you can create an organization meeting. Stand where you want to hold a meeting by filling out the forms, the organization's players will be notified."
-ORGS_Lang.meetexplain1 = "Small meeting description"
-ORGS_Lang.meetexplain2 = "Where is the meeting? (For example: Opposite the PU)"
+ORGS_Lang.meetexplain = "Here you can create a meeting of the organization. Stand where you want to meet filling in forms, the players of the organization will be notified."
+ORGS_Lang.meetexplain1 = "a short description of the meeting"
+ORGS_Lang.meetexplain2 = "Where is the meeting? (For example: Near the PD)"
 ORGS_Lang.meetcreate = "Create a meeting!"
-ORGS_Lang.meetset = "Created an organization meeting at"
+ORGS_Lang.meetset = "Created an organization meeting in "
 
 --[[ NPC ]]--
 
-ORGS_Lang.npcLine = "Hello. Have you come to create or leave a gang? Creation of a gang costs $ 100,000. Choose what you want below. \ NHave a nice day!"
-ORGS_Lang.npcleave = "I want to quit the gang."
-ORGS_Lang.npcnew = "I want to create a gang. (Cost 100k $)"
+ORGS_Lang.npcLine = "what you need?"
+ORGS_Lang.npcleave = "I want to quit the organization."
+ORGS_Lang.npcnew = "I want to create a organization. (Cost 100k$)"
 ORGS_Lang.npcgetmoney = "Earn money first. You can't afford it."
-ORGS_Lang.npccallorg = "Ok, you want to create a new organization. What will you name it ?? \ n \ nChoose a name after thinking, you cannot change it later!"
-ORGS_Lang.npcnametext = "Please enter a name for the new organization ..."
+ORGS_Lang.npccallorg = "OK, you want to create a new organization. What do you call it??\n\nselect the name after thinking, you will not be able to change it later!"
+ORGS_Lang.npcnametext = "Enter the name of the new organization..."
 ORGS_Lang.npccreate = "Create a new organization."
